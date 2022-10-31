fc4b61167d568241cf3f3d14ed868499c92476f0
김영훈 <joseph0926@naver.com>
2022-10-31 10:33:17 +0900

    2022.10.31

---
+++ ch01/app.ts
@@ -0,0 +1,12 @@
+const add = (n1: number, n2: number) => {
+  // if (typeof n1 !== "number" || typeof n2 !== "number") {
+  //   throw new Error("올바르지 않은 타입!");
+  // }
+  return n1 + n2;
+};
+
+const number1 = 5;
+const number2 = 2.8;
+
+const result = add(number1, number2);
+console.log(result);
